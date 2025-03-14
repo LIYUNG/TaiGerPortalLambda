daceae2174e190753f010e19f9ceacb4af2cc729
Leo <liyung.chen@tum.de>
2025-03-14 19:26:33 +0100

    update stack name

--- lib/app-stage.ts
+++ lib/app-stage.ts
@@ -1,6 +1,7 @@
 import { Construct } from "constructs";
 import { LambdaStack } from "./lambda-stack";
 import { Stage, StageProps } from "aws-cdk-lib";
+import { APP_NAME_TRANSCRIPT_ANALYZER } from "../configuration";
 
 interface DeploymentkProps extends StageProps {
     stageName: string;
@@ -15,14 +16,18 @@ export class PipelineAppStage extends Stage {
     constructor(scope: Construct, id: string, props: DeploymentkProps) {
         super(scope, id, props);
 
-        const lambdaStack = new LambdaStack(this, `LambdaStack-${props.stageName}`, {
-            env: props.env,
-            stageName: props.stageName,
-            domainStage: props.domainStage,
-            isProd: props.isProd,
-            mongodbUriSecretName: props.mongodbUriSecretName,
-            mongoDBName: props.mongoDBName,
-            fileS3BucketName: props.fileS3BucketName
-        });
+        const lambdaStack = new LambdaStack(
+            this,
+            `${APP_NAME_TRANSCRIPT_ANALYZER}LambdaStack-${props.stageName}`,
+            {
+                env: props.env,
+                stageName: props.stageName,
+                domainStage: props.domainStage,
+                isProd: props.isProd,
+                mongodbUriSecretName: props.mongodbUriSecretName,
+                mongoDBName: props.mongoDBName,
+                fileS3BucketName: props.fileS3BucketName
+            }
+        );
     }
 }
